c5199c4ea6a4f92917d0d39b72e96f428b9e4b9c
akihito <kohiga@gmail.com>
2024-06-06 02:31:58 +0900

    added chart js

--- realtimeplot.py
+++ realtimeplot.py
@@ -1,5 +1,8 @@
 import argparse
 import serial
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import argparse
 
 parser = argparse.ArgumentParser(description="realtime plot app on a web browser")
 parser.add_argument('-d', '--device', type=str, default='/dev/serial0', help='a device file connected to arduino')
@@ -7,6 +10,15 @@ args = parser.parse_args()
 
 readSer = serial.Serial(args.device, 9600, timeout=3)
 
-while(True):
-    value = int(readSer.readline().strip())
+def bgTask():
+    value = float(readSer.readline().strip())
     print(value)
+
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    socketio = SocketIO(app)
+
+    @app.route("/chart")
+    def hello_world():
+        return render_template('chart.html')
